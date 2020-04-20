e77f14ecb7c037c312fcc94c1687afb979eba606
vatnikchernovenkiss <56398346+vatnikchernovenkiss@users.noreply.github.com>
2020-04-20 22:37:07 +0300

    hw2_fix

--- src/storage/ThreadSafeSimpleLRU.h
+++ src/storage/ThreadSafeSimpleLRU.h
@@ -1,3 +1,4 @@
+ 
 #ifndef AFINA_STORAGE_THREAD_SAFE_SIMPLE_LRU_H
 #define AFINA_STORAGE_THREAD_SAFE_SIMPLE_LRU_H
 
@@ -22,42 +23,36 @@ public:
 
     // see SimpleLRU.h
     bool Put(const std::string &key, const std::string &value) override {
-        // TODO: sinchronization
-	 std::lock_guard<std::mutex> lock(mut);
+        std::lock_guard<std::recursive_mutex> lock(mut);
         return SimpleLRU::Put(key, value);
     }
 
     // see SimpleLRU.h
     bool PutIfAbsent(const std::string &key, const std::string &value) override {
-        // TODO: sinchronization
-	std::lock_guard<std::mutex> lock(mut);
+        std::lock_guard<std::recursive_mutex> lock(mut);
         return SimpleLRU::PutIfAbsent(key, value);
     }
 
     // see SimpleLRU.h
     bool Set(const std::string &key, const std::string &value) override {
-        // TODO: sinchronization
-	std::lock_guard<std::mutex> lock(mut);
+        std::lock_guard<std::recursive_mutex> lock(mut);
         return SimpleLRU::Set(key, value);
     }
 
     // see SimpleLRU.h
     bool Delete(const std::string &key) override {
-        // TODO: sinchronization
-	std::lock_guard<std::mutex> lock(mut);
+        std::lock_guard<std::recursive_mutex> lock(mut);
         return SimpleLRU::Delete(key);
     }
 
     // see SimpleLRU.h
     bool Get(const std::string &key, std::string &value) override {
-        // TODO: sinchronization
-	std::lock_guard<std::mutex> lock(mut);
+        std::lock_guard<std::recursive_mutex> lock(mut);
         return SimpleLRU::Get(key, value);
     }
 
 private:
-    // TODO: sinchronization primitives
-    std::mutex mut;
+    std::recursive_mutex mut;
 };
 
 } // namespace Backend
